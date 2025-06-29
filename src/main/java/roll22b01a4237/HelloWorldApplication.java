2fd3ddebfa3751600742dc64a2f930b56b59bd88
amulya <nagaamulyachodisetty@gmail.com>
2025-06-29 21:25:19 +0530

    Added numbers endpoint

--- src/main/java/roll22b01a4237/HelloWorldApplication.java
+++ src/main/java/roll22b01a4237/HelloWorldApplication.java
@@ -5,17 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
-}
 
-@RestController
-class HelloController {
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/numbers")
+    public List<Integer> getNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+        return numbers;
     }
 }
